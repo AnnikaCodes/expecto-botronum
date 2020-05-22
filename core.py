d7d60cca087bd0607cde5022867dddbd0281c8a3
Annika <hehe@doesntexist.com>
2020-05-22 12:50:18 -0700

    core: basic skeleton of Room class

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import config
+import re
 
 ################## core.py #####################
 ## core functionality of Expecto Botronum     ##
@@ -19,3 +20,26 @@ def log(message):
         print(message)
     elif message[:2] == 'I:' and config.loglevel >= 2:
         print(message)
+
+def toID(string):
+    return re.sub('[^0-9a-zA-Z]+', '', string).lower()
+
+################
+## Room Class ##
+################
+
+class Room():
+    def __init__(self, name):
+        self.auth = self.updateAuth()
+        self.id = toID(name)
+
+    def updateAuth(self):
+        log("W: Room.updateAuth() isn't implemented yet")
+        return {'+': [], '%': [], '@': [], '#': []}
+
+    def say(self, message):
+        log("W: Room.say() isn't implemented yet")
+
+    def leave(self):
+        log("W: Room.leave() isn't implemented yet")
+    
