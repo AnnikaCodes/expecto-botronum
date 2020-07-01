97bf713f0542b479312b2460fe3edcef89a28b1c
Annika <hehe@doesntexist.com>
2020-06-30 19:06:02 -0700

    Hopefully make GitHub actions work properly

--- test.sh
+++ test.sh
@@ -4,16 +4,19 @@ echo "Linting code..."
 pylint *.py */*.py */*/*.py --disable=R,fixme || pylint-exit -wfail -efail -cfail $?
 LINT_SUCCESS=$?
 echo "Running tests..."
-pytest
+pytest tests/
 TEST_SUCCESS=$?
 
 if [ $LINT_SUCCESS == 0 ] && [ $TEST_SUCCESS == 0 ]; then
     echo "Success!"
-    exit
+    exit 0
 elif [ $LINT_SUCCESS == 0 ]; then
     echo "Linting passed, but tests failed."
+    exit 1
 elif [ $TEST_SUCCESS == 0 ]; then
     echo "Tests passed, but linting failed."
+    exit 1
 else
     echo "Both tests and linting failed :c"
+    exit 2
 fi
